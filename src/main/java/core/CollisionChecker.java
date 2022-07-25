96cc1543244875da7b3ce9cc5005e93dd54bd67d
PaqAttack <cpaquin966@gmail.com>
2022-07-25 14:17:19 +0100

    Implemented Collision Checking Class to handle movement.

---
+++ src/main/java/core/CollisionChecker.java
@@ -0,0 +1,69 @@
+package core;
+
+import entity.Entity;
+
+public class CollisionChecker {
+
+    private GamePanel gamepanel;
+
+    public CollisionChecker(GamePanel gamePanel) {
+        this.gamepanel = gamePanel;
+    }
+
+    public void checkTile(Entity entity) {
+        int eLeftWorldX = entity.getWorldX() + entity.getCollisionBoundsX();
+        int eRightWorldX = entity.getWorldX() + entity.getCollisionBoundsX() + entity.getCollisionBoundsWidth();
+        int eTopWorldY = entity.getWorldY() + entity.getCollisionBoundsY();
+        int eBottomWorldY = entity.getWorldY() + entity.getCollisionBoundsY() + entity.getCollisionBoundsHeight();
+
+        int eLeftCol = eLeftWorldX / ScreenVar.TILE_SIZE.getValue();
+        int eRightCol = eRightWorldX / ScreenVar.TILE_SIZE.getValue();
+        int eTopRow = eTopWorldY / ScreenVar.TILE_SIZE.getValue();
+        int eBottomRow = eBottomWorldY / ScreenVar.TILE_SIZE.getValue();
+
+        int tile1, tile2;
+
+        switch (entity.getDirection()) {
+            case UP: {
+                eTopRow = (eTopWorldY - Entity.CollisionCheckDistance) / ScreenVar.TILE_SIZE.getValue();
+                tile1 = gamepanel.getTileManager().getMap()[eLeftCol][eTopRow];
+                tile2 = gamepanel.getTileManager().getMap()[eRightCol][eTopRow];
+                if (!gamepanel.getTileManager().getTileByIndex(tile1).isWalkable() ||
+                        !gamepanel.getTileManager().getTileByIndex(tile2).isWalkable()) {
+                    entity.setCollisionHappening(true);
+                }
+                break;
+            }
+            case DOWN: {
+                eBottomRow = (eBottomWorldY + Entity.CollisionCheckDistance) / ScreenVar.TILE_SIZE.getValue();
+                tile1 = gamepanel.getTileManager().getMap()[eLeftCol][eBottomRow];
+                tile2 = gamepanel.getTileManager().getMap()[eRightCol][eBottomRow];
+                if (!gamepanel.getTileManager().getTileByIndex(tile1).isWalkable() ||
+                        !gamepanel.getTileManager().getTileByIndex(tile2).isWalkable()) {
+                    entity.setCollisionHappening(true);
+                }
+                break;
+            }
+            case RIGHT: {
+                eRightCol = (eRightWorldX + Entity.CollisionCheckDistance) / ScreenVar.TILE_SIZE.getValue();
+                tile1 = gamepanel.getTileManager().getMap()[eRightCol][eTopRow];
+                tile2 = gamepanel.getTileManager().getMap()[eRightCol][eBottomRow];
+                if (!gamepanel.getTileManager().getTileByIndex(tile1).isWalkable() ||
+                        !gamepanel.getTileManager().getTileByIndex(tile2).isWalkable()) {
+                    entity.setCollisionHappening(true);
+                }
+                break;
+            }
+            case LEFT: {
+                eLeftCol = (eLeftWorldX - Entity.CollisionCheckDistance) / ScreenVar.TILE_SIZE.getValue();
+                tile1 = gamepanel.getTileManager().getMap()[eLeftCol][eTopRow];
+                tile2 = gamepanel.getTileManager().getMap()[eLeftCol][eBottomRow];
+                if (!gamepanel.getTileManager().getTileByIndex(tile1).isWalkable() ||
+                        !gamepanel.getTileManager().getTileByIndex(tile2).isWalkable()) {
+                    entity.setCollisionHappening(true);
+                }
+                break;
+            }
+        }
+    }
+}
