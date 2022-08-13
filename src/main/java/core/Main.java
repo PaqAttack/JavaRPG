208ef14da373cffe4298d3bff779a0650add27f1
PaqAttack <cpaquin966@gmail.com>
2022-08-13 17:42:08 +0100

    Added support for args to control logger

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -1,15 +1,24 @@
 package core;
 
 import javax.swing.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
+
+    private static final Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
     /**
      * Would you like to play a game?
      *
-     * @param args Not supported
+     * @param args commands to adjust program operation.
+     *             -ClearLog will clear old log file data
      */
     public static void main(String[] args) {
-        // Create and setup a Frame (Stage)
+        // Read command line arguments (if any)
+        processArgs(args);
+
+        // Create and set up a Frame (Stage)
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setResizable(false);
@@ -31,5 +40,16 @@ public class Main {
         // Lets get this game thread started.
         //TODO: Start paused and show menu (Not yet implemented)
         gamePanel.startGameThread();
+    }
+
+    private static void processArgs(String[] args) {
+        for (String s : args) {
+            if (s.equalsIgnoreCase("-ClearLog")) {
+                LogTest.setDoAppend(false);
+            } else {
+                String msg = String.format("%s : %s", "Unknown argument", s);
+                logger.log(Level.INFO, msg);
+            }
+        }
     }
 }
